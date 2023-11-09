6159ff374ee10e5b1b37faa54e8dd3573bfcd213
Arthur Santos <santos.santos86@hotmail.com>
2023-11-08 21:32:02 -0300

    initial configs

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,17 @@
+export const theme = {
+    colors: {
+        primary: '#007BFF',
+        secondary: '#6C757D',
+        background: '#F8F9FA',
+    },
+    fontSizes: {
+        small: '2rem',
+        medium: '3rem',
+        large: '5rem',
+    },
+    sizes: {
+        small: '2rem',
+        medium: '4rem',
+        large: '6rem',
+    },
+}
